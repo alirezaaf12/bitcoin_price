3079c32db1ec2f9d38e207f8794ea8681ad1771c
Alireza <60536774+alirezaaf12@users.noreply.github.com>
2021-01-21 00:00:44 +0330

    Add files via upload

---
+++ bitcoin_price_show.py
@@ -0,0 +1,17 @@
+import requests
+
+
+# you must connect to tor before using this script
+# if you are not in Iran, you can comment out proxy line and delete proxy arg for get method
+def extract_bit_price():
+    proxy = {"http" : "socks5://127.0.0.1:9050", "https" : "socks5://127.0.0.1:9050"}
+
+    res = requests.get(url="https://api.coindesk.com/v1/bpi/currentprice.json", proxies=proxy)
+
+
+    bitcoin_price = (res.json()["bpi"]["USD"]["rate"])
+
+    print("Bitcoin price is %s$" % bitcoin_price)
+
+if __name__ == '__main__':
+    extract_bit_price()
